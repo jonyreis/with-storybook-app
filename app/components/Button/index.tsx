a1c6caf4a2807becef42427466c0669cee8b3474
jony <jonyreiscardoso@gmail.com>
2023-06-27 18:20:44 -0300

    feat: add button component

---
+++ app/components/Button/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = () => {
+    return (
+        <button>Jony Reis</button>
+    )
+}
+
+export default Button
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import Button from './components/Button'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,11 +9,9 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
+        <p>Jony Reis</p>
         <div>
+          <Button />
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
